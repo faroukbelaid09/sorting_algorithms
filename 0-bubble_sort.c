28dbf4fbe71aa91fcbe895bf9c2b05ad5d7b9488
Farouk <faroukbelaid9@gmail.com>
2024-02-17 05:59:30 -0800

    first task in sorting project

---
+++ 0-bubble_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swapNumbers - Swap 2 integers in array
+ * @firstInt: First integer
+ * @secondInt: Second integer
+ */
+void swapNumbers(int *firstInt, int *secondInt)
+{
+	int tmp;
+
+	tmp = *firstInt;
+	*firstInt = *secondInt;
+	*secondInt = tmp;
+}
+
+/**
+ * bubble_sort - Function sorts array of integers in ascending order
+ * @array: An array of integers
+ * @size: The size of the array
+ *
+ * Description: Prints the array after each swap
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t length = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < length  - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swapNumbers(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		length--;
+	}
+}
